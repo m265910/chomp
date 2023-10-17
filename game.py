8277c1ed714a15d1e7089e75f863653989cebf1f
m265910 <148266374+m265910@users.noreply.github.com>
2023-10-17 15:04:31 -0400

    change

--- game.py
+++ game.py
@@ -1,1 +1,28 @@
-print('Welcome to my code!')
+import pygame
+import sys
+
+pygame.init()
+
+screen_width = 300
+screen_height = 300
+
+blue = (0, 0, 255)
+brown = (200, 150, 100)
+
+screen = pygame.display.set_mode((screen_width, screen_height))
+pygame.display.set_caption("Blue Background with Brown Rectangle")
+
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+    screen.fill(brown)
+
+    rectangle_height = 200
+    pygame.draw.rect(screen, blue, (0, 50, screen_width, rectangle_height))
+
+    pygame.display.flip()
+
+pygame.quit()
+sys.exit()
